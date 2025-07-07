f998b56f04d201aae3103cd06d52c395304cbcb0
sumukha s Hebbar <sumukhashebbar15@gmail.com>
2025-07-07 17:20:15 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,17 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      // Fixes npm packages that depend on `async_hooks`
-      config.resolve.fallback = {
-        ...(config.resolve.fallback || {}),
-        async_hooks: false,
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
