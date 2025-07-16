b4f53ba787f90fe1909ebbe272da541645337b33
sumukha s Hebbar <sumukhashebbar15@gmail.com>
2025-07-16 07:00:51 +0000

    in the active emergency incidents should contain a column called site_id

--- src/app/towerco/home/page.tsx
+++ src/app/towerco/home/page.tsx
@@ -193,11 +193,12 @@ export default function TowercoHomePage() {
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead>Site</TableHead>
+                  <TableHead>Site Name</TableHead>
+                  <TableHead>Site ID</TableHead>
                   <TableHead>Agency</TableHead>
                   <TableHead>Patrolling Officer</TableHead>
                   <TableHead>Guard</TableHead>
-                  <TableHead>Date</TableHead>
+                  <TableHead>Date & Time</TableHead>
                   <TableHead>Contact</TableHead>
                 </TableRow>
               </TableHeader>
@@ -215,12 +216,13 @@ export default function TowercoHomePage() {
                       <TableCell className="font-medium">
                         {siteDetails?.name || 'N/A'}
                       </TableCell>
+                       <TableCell>{incident.siteId}</TableCell>
                       <TableCell>{agencyDetails?.name || 'N/A'}</TableCell>
                       <TableCell>
                         {patrollingOfficerDetails?.name || 'N/A'}
                       </TableCell>
                       <TableCell>{guardDetails?.name || 'N/A'}</TableCell>
-                      <TableCell>{new Date(incident.incidentTime).toLocaleDateString()}</TableCell>
+                      <TableCell>{new Date(incident.incidentTime).toLocaleString()}</TableCell>
                       <TableCell>
                         <DropdownMenu>
                           <DropdownMenuTrigger asChild>
