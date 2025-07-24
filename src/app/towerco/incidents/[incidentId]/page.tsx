f5fff9a52eb44fa64d393db1af5349c2f66b5bef
sumukha s Hebbar <sumukhashebbar15@gmail.com>
2025-07-24 18:42:45 +0000

    in the incident report make the 4 cards come in hte same line as there i

--- src/app/towerco/incidents/[incidentId]/page.tsx
+++ src/app/towerco/incidents/[incidentId]/page.tsx
@@ -201,7 +201,7 @@ export default function IncidentReportPage() {
         </Button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {site && (
           <Card>
             <CardHeader>
