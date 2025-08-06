e6e33bda4e839a0876bab80eeef4f67d24df6779
sumukha s Hebbar <sumukhashebbar15@gmail.com>
2025-08-06 07:14:34 +0000

    we dont need that in this right so remove it

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/summarize-emergency-call.ts';
-import '@/ai/flows/analyze-selfie-compliance.ts';
-import '@/ai/flows/generate-selfie-request-message.ts';
