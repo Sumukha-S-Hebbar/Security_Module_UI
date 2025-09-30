4144466804e477b0453873fd0af36691b3c8d539
sumukha s Hebbar <sumukhashebbar15@gmail.com>
2025-09-30 11:58:35 +0000

    i am facing the same issue that i faced the scroll bar is not coming up

--- src/app/agency/sites/[siteId]/page.tsx
+++ src/app/agency/sites/[siteId]/page.tsx
@@ -529,7 +529,7 @@ export default function AgencySiteReportPage() {
           {isIncidentsLoading ? (
             <div className="flex items-center justify-center p-10"><Loader2 className="w-8 h-8 animate-spin" /></div>
           ) : paginatedIncidents && paginatedIncidents.results.length > 0 ? (
-            <ScrollArea className="max-h-72 h-auto">
+            <ScrollArea className="h-72">
               <Table>
                 <TableHeader>
                   <TableRow>
